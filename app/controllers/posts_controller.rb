0562aa03e27419e85596a1ce22fbb4763bdf3693
aronDillQs5328 <gam3.aronDill@gmail.com>
2017-11-13 20:04:30 +0200

    added more new files on lection

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,47 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!
+before_action :set_post, only:[:show, :edit, :update, :destroy]
 
+  def show
+    pp @comment = @post.postcomments
+  end
 
   def new
-  	@post = Post.new
+    @post = Post.new
   end
 
   def create
-  	@post = current_user.posts.build(post_params)
-  	if @post.save
-  		redirect_to root_path
-  	else 
-  	render 'new'	
-  	end	
-  		
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+    else 
+    render 'new'  
+    end 
+      
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def update
-    @post = Post.find(params[:id])
-  	@post.update(post_params)
-  	redirect_to root_path
+    @post.update(post_params)
+    redirect_to root_path
   end
 
   def destroy
-    @post = Post.find(params[:id])
-  	@post.destroy
-  	redirect_to root_path
+    @post.destroy
+    redirect_to root_path
   end
 
 private
 
-	def post_params
-		params.require(:post).permit(:user_id, :title, :body)
-	end
+  def post_params
+    params.require(:post).permit(:user_id, :title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#index"
 
-  resources :posts
-  	
+  resources :posts do
+  	resources :postcomments
+  end	
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
